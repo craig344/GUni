cf99d232b3f257f5b3502577e5f5319986f56551
Craig Da Silva <craig_344@hotmail.com>
2018-07-11 15:17:44 +0530

    Bug fixes

--- bubble sort/src/application/bubbleSort.java
+++ bubble sort/src/application/bubbleSort.java
@@ -1,20 +1,26 @@
 package application;
 
-
 public class bubbleSort {
+	
 	public static void main(String[] args) {
 		int arr[] = {10, 3, 9};
 		System.out.println("Before: ");
-		System.out.println(arr);
+		for(int i = 0; i < arr.length; i++) {
+		      System.out.print(arr[i] + " ");
+		}
 		
-		for (int i = 0; i < arr.length; i++) {
-			for (int j = 0; j < arr.length; j++) {
+		for (int i = 0; i < arr.length - 1; i++) {
+			for (int j = 0; j < arr.length - 1; j++) {
 				if(arr[j] > arr[j+1]) {
 					Integer temp = arr[j];
 					arr[j] = arr[j+1];
 					arr[j+1] = temp;
 				}
 			}
+		}
+		System.out.println("\nAfter: ");
+		for(int i = 0; i < arr.length; i++) {
+		      System.out.print(arr[i] + " ");
 		}
 	}
 }
